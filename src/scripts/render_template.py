2760afff8d819746f8a402d4d4a1be0749211168
jtreutel <jennings@circleci.com>
2021-06-14 13:02:26 +0900

    A million bugfixes.

--- src/scripts/render_template.py
+++ src/scripts/render_template.py
@@ -1,32 +1,32 @@
-import re, os, glob, yaml
-from sys import exit
+import re, os, glob, yaml, sys
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('')) #default dir is ./templates
+env = Environment(loader=FileSystemLoader(''))
 
+#TODO: Fix this, it doesn't work
 def get_templates_in_dir(path):
   file_list = []
-  stripped_path = path.rstrip("/")
   for extension in ["j2", "jinja"]:
-    file_list += glob.glob(f'%{stripped_path}/*.{extension}')
+    file_list += glob.glob(f'{path}/*.{extension}')
   return file_list 
 
-#Use the path and outpute directory supplied by the orb arguments
-templates_to_render = [sys.argv[0]]
-output_dir = sys.argv[1]
+#Use the path and output directory supplied by the orb arguments
+supplied_path = sys.argv[1].rstrip("/")
+output_dir = sys.argv[2].rstrip("/")
 
-#Check if the supplied path exists
-if not os.path.isfile(templates_to_render[0]):
-  print("No file/directory found at path %s!" % (templates_to_render[0]))
+
+templates_to_render = []
+if os.path.isfile(supplied_path):
+  templates_to_render.append(supplied_path)
+# If supplied path is a directory, scan it for .j2 and .jinja files
+elif os.path.isdir(supplied_path):
+  print("It's a directory!") #DEBUG
+  templates_to_render = get_templates_in_dir(supplied_path)
+  print(templates_to_render)
+else:
+  print("No file/directory found at path \"%s\"!" % (supplied_path))
   sys.exit(1)
 
-# If supplied path is a directory, scan it for .j2 and .jinja files
-if os.path.isdir(templates_to_render):
-  templates_to_render = get_templates_in_dir(templates_to_render)
-
-# Get all env vars in the machine/container for use in Jinja template rendering
-# Convert the os.environ object to a dict, then format each env var assignment like so: FOO="bar"
-templatevars = ', '.join(f'{k}="{v}"' for k, v in dict(os.environ).items())
 
 #Create the output directory if it doesn't already exist
 if not os.path.exists(output_dir):
@@ -36,10 +36,10 @@ if not os.path.exists(output_dir):
 for template_path in templates_to_render:
     template = env.get_template(template_path)
 
-    parsed_template = template.render(templatevars)
+    parsed_template = template.render(os.environ) #templatevars)
 
-    #Write rendered template to ./rendered/foo.tf
-    #TODO: figure out regex
-    filename = re.search(r"(?<=\.\/templates\/).*?(?=\.j2)", template_path).group(0)
-    with open(''.join(["./rendered/", filename]), "w") as fh:
+    #Write rendered template to output_dir
+    #group(0) gets the filepath minus jinja extension; group(2) gets just the filename
+    filename = re.search(r"(.*\/)(.*?)(?=(\.jinja)|(\.j2))", template_path).group(2)
+    with open('/'.join([output_dir, filename]), "w") as fh:
         fh.write(parsed_template)
